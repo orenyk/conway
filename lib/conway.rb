889dcd60832b70543b5827b7a6b72566badf19b9
Oren Kanner <okanner@gmail.com>
2017-10-27 14:54:14 -0400

    Initial work with @melina-delgado

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,5 +1,49 @@
 require "conway/version"
 
-module Conway1
-  # Your code goes here...
+class Conway
+
+  attr_accessor :board
+
+  def initialize(board)
+    @board = Board.new(board)
+  end
+
+  def new_board(rounds)
+    rounds.times { @board = BoardUpdater.iterate(@board) }
+  end
+end
+
+class Board
+
+  attr_reader :height, :width, :grid
+
+  def initialize(board_array)
+    validate_board(board_array)
+    @height = board_array.length
+    @width = board_array.first.length
+    @grid = board_array
+  end
+
+  private
+
+  def validate_board(board)
+    is_rectangular?(board)
+    is_binary?(board)
+  end
+
+  def is_rectangular?(board)
+    width = board.first.length
+    return if board.all? { |r| r.length == width }
+    raise ArgumentError, "Board must be rectangular"
+  end
+
+  def is_binary?(board)
+    flat = board.flatten
+    return if flat.all? { |c| (0..1).to_a.include?(c) }
+    raise ArgumentError, "Board must only contain zeros and ones"
+  end
+end
+
+class BoardUpdater
+
 end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -5,7 +5,64 @@ RSpec.describe Conway do
     expect(Conway1::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "transforms the board correctly" do
+    board = [[0, 1, 0],[1, 0, 0],[0, 1,0]]
+    game = Conway.new(board)
+    expected = [[0,0,0],[1,1,0],[0,0,0]]
+    expect(game.new_board(1)).to eq expected
+  end
+
+  describe "#new_board" do
+    it "calls BoardUpdater#iterate the right number of times" do
+      board = [[0]]
+      game = Conway.new(board)
+      allow(BoardUpdater).to receive(:iterate)
+      game.new_board(1)
+      expect(BoardUpdater).to have_received(:iterate).exactly(1).times
+    end
   end
 end
+
+RSpec.describe Board do
+  describe "#initialize" do
+    it "properly sets width" do
+      board_array = [[1,1],[0,0]]
+      board = described_class.new(board_array)
+      expect(board.width).to eq(2)
+    end
+
+    it "properly sets height" do
+      board_array = [[1,1],[0,0],[0,1]]
+      board = described_class.new(board_array)
+      expect(board.height).to eq(3)
+    end
+
+    it "properly sets grid" do
+      board_array = [[0]]
+      board = described_class.new(board_array)
+      expect(board.grid).to eq(board_array)
+    end
+
+    it "rejects non-rectangular arrays" do
+      board_array = [[0,0],[0,1,1]]
+      expect{ described_class.new(board_array) }.to raise_error(ArgumentError)
+    end
+
+    it "rejects cells not zeros or ones" do
+      board_array = [[2]]
+      expect{ described_class.new(board_array) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#neighbors" do
+    xit "returns an array of neighbors"
+  end
+end
+
+# RSpec.describe BoardUpdater do
+#   describe "#iterate" do
+#     it "calls BoardUpdater#update_cell the expected number of times" do
+#       board = [[1, 0],[1, 1]]
+#     end
+#   end
+# end
